7498d6d8c2789d5d608936e0d9bd8d4c1bba1d90
axelgaffer <axelgaffer@gmail.com>
2017-10-23 17:09:17 -0400

    It asks for input
    Scanner input works to ask their name

--- hello.java
+++ hello.java
@@ -1,16 +1,14 @@
+// Alex Gaffney
+// github assignment
 // a simple program to practice common Github commands 
-
+package github_assignment;
+import java.util.Scanner;
 public class hello {
-
     public static void main(String [] args) {
-
-
 	// added  line	
-
-        System.out.println("Hello Class!");
-
-
-
+    	System.out.println("Enter your name:");
+    	Scanner scan = new Scanner(System.in);
+    	String name = scan.next();
+    	System.out.println("Hello " + name + ", my name is Alex");
     }
 }
-
